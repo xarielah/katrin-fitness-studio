8d67a5b0e4cab2a566946b8607e6d9177a97e728
Shahar Yair <shaharyair12@gmail.com>
2024-03-08 20:21:00 +0200

    component: add loading screen

---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,62 @@
+import { AnimatePresence, motion } from "framer-motion";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const loadingScreenVariants = {
+  visible: { opacity: 1 },
+  exit: { opacity: 0, transition: { ease: "linear", duration: 0.5 } },
+};
+
+const spinnerVariants = {
+  animate: {
+    rotate: 360,
+    transition: {
+      ease: "linear",
+      repeat: Infinity,
+      duration: 0.6,
+    },
+  },
+};
+
+export default function LoadingScreen() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout();
+  }, []);
+
+  return (
+    <>
+      <AnimatePresence mode="wait">
+        {loading && (
+          <motion.div
+            variants={loadingScreenVariants}
+            animate="visible"
+            exit="exit"
+            className="fixed left-0 top-0 z-[60] flex h-full w-full items-center justify-center bg-[#bba187]"
+          >
+            <div className="relative w-3/5 rounded-full lg:w-1/6">
+              <Image
+                src={"/loadingScreen/circleLogo.webp"}
+                width={500}
+                height={500}
+                alt="Loading Screen"
+                priority={true}
+                className="aspect-square w-full rounded-full object-cover object-center drop-shadow-lg"
+              />
+              <motion.div
+                variants={spinnerVariants}
+                animate="animate"
+                className="absolute left-[-2.5%] top-[-2.5%] h-[105%] w-[105%] rounded-full border-[3px] border-r-[6px] border-[#ebebeb3b] border-r-white"
+              />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
+  );
+}
