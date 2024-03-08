38e880b57f9fa2d9499afe7235c0a6f470b09666
Shahar Yair <shaharyair12@gmail.com>
2024-03-08 20:26:04 +0200

    app: add components, set font and update favicon
    - add navbar, footer and loading screen
    - set app font to assistant

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
+import Footer from "@/components/Footer";
+import LoadingScreen from "@/components/LoadingScreen";
+import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
+import { Assistant } from "next/font/google";
+import Head from "next/head";
+
+const assistant = Assistant({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={assistant.className}>
+      <Head>
+        <link rel="icon" href="/logo.svg" sizes="any" />
+      </Head>
+      <LoadingScreen />
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </main>
+  );
 }
