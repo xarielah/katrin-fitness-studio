4c2929864a3b80cee2d1fff2588f2d167b890790
Shahar Yair <shaharyair12@gmail.com>
2024-03-09 16:38:57 +0200

    package: add vercel speed insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,7 @@ import LoadingScreen from "@/components/LoadingScreen";
 import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Assistant } from "next/font/google";
 import Head from "next/head";
 
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }) {
         <link rel="icon" href="/logo.svg" sizes="any" />
       </Head>
       <Analytics />
+      <SpeedInsights />
       <LoadingScreen />
       <Navbar />
       <Component {...pageProps} />
