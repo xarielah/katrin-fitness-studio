0f65a348746d91054ae66ab535b71670eba2e8c4
Shahar Yair <shaharyair12@gmail.com>
2024-03-11 14:50:17 +0200

    server: add sanity client intergration and image builder
    - add sanity domain into images remote patterns
    - install next-sanity, sanity/image-url

--- next.config.js
+++ next.config.js
@@ -7,6 +7,14 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: "",
+        pathname: `/images/${process.env.NEXT_PUBLIC_SANITY_PROJECT_ID}/**`,
+      },
+    ],
   },
 
   // Rewrite non existing paths to the root page
